a7c95499b6a6280085ec39e5de15f14e7f5b7ec9
sonhai32 <lamsonhai1379@gmail.com>
2020-09-03 15:52:49 +0700

    add Modal NhomTK

---
+++ QuanLyBanHangOnline/Modal/NhomTK.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHangOnline.Modal
+{
+    class NhomTK
+    {
+        private int maNhom;
+        private String tenNhom;
+        private String ghiChu;
+
+        public NhomTK(int maNhom, string tenNhom, string ghiChu)
+        {
+            this.maNhom = maNhom;
+            this.tenNhom = tenNhom;
+            this.ghiChu = ghiChu;
+        }
+
+        public int MaNhom { get => maNhom; set => maNhom = value; }
+        public string TenNhom { get => tenNhom; set => tenNhom = value; }
+        public string GhiChu { get => ghiChu; set => ghiChu = value; }
+    }
+}
