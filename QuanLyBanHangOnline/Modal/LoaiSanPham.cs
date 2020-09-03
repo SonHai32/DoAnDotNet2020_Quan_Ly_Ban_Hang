43fc98273a49aad68e0d7c457e5b1e487752699c
sonhai32 <lamsonhai1379@gmail.com>
2020-09-03 15:43:33 +0700

    add Modal LoaiSanPham

---
+++ QuanLyBanHangOnline/Modal/LoaiSanPham.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHangOnline.Modal
+{
+    class LoaiSanPham
+    {
+        private int maLoai;
+        private String loaiSP;
+        private String ghiChu;
+
+        public LoaiSanPham(int maLoai, string loaiSP, string ghiChu)
+        {
+            this.maLoai = maLoai;
+            this.loaiSP = loaiSP;
+            this.ghiChu = ghiChu;
+        }
+
+        public int MaLoai { get => maLoai; set => maLoai = value; }
+        public string LoaiSP { get => loaiSP; set => loaiSP = value; }
+        public string GhiChu { get => ghiChu; set => ghiChu = value; }
+    }
+}
